f1f07cd2a7d7b6b028b6f504751286efc596b31a
hopin22 <sprzedawca705@gmail.com>
2025-05-28 14:06:43 +0000

    zad1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
+
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-  Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     System.out.print("Ile liczb chcesz wprowadzić?");
-      int n = scanner.nextInt();
+    int n = scanner.nextInt();
     int[] liczby = new int[n];
 
     System.out.println("Wprowadź " + n + " liczby:");
@@ -15,18 +16,17 @@ public class Main {
     int max = liczby[0];
     int min = liczby[0];
 
-    for(int i = 1; i < liczby.length; i++){
-      if(liczby[i] > max){
+    for (int i = 1; i < liczby.length; i++) {
+      if (liczby[i] > max) {
         max = liczby[i];
       }
-      if(liczby[i] < min){
+      if (liczby[i] < min) {
         min = liczby[i];
       }
     }
     int roznica = max - min;
     System.out.println("Różnica między największą i najmniejszą liczbą to: " + roznica);
-     
+
   }
 
-  
 }
